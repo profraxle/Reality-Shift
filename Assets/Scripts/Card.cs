6794827e67b29c35224b4b808b215172e9a66b76
AbertayFraxle <91606018+AbertayFraxle@users.noreply.github.com>
2024-11-06 13:47:10 +0000

    putting prior project to repository

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+
+    Texture2D cardImage;
+    string cardName;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+}
---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+    private List<Card> decklist;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DrawFromDeck()
+    {
+
+    }
+
+
+}
